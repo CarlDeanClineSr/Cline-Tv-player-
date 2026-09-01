cfba2a8b1f96e99a978786c145da2284a1b46738
Carl Dean Cline Sr <CARLDCLINE@GMAIL.COM>
2026-09-01 08:53:49 -0500

    Add verified Man from Atlantis harvested catalog

---
+++ harvest_man_from_atlantis.js
@@ -0,0 +1,16 @@
+// Generated from verified Colab ranged-playback results.
+const HARVEST_MAN_FROM_ATLANTIS = [
+    {n: "Man from Atlantis S01E01 - Melt Down", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E01%20-%20Melt%20Down.mp4"},
+    {n: "Man from Atlantis S01E02 - The Mudworm", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E02%20-%20The%20Mudworm.mp4"},
+    {n: "Man from Atlantis S01E03 - Hawk Of Mu", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E03%20-%20Hawk%20Of%20Mu.mp4"},
+    {n: "Man from Atlantis S01E04 - Giant", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E04%20-%20Giant.mp4"},
+    {n: "Man from Atlantis S01E05 - Man O'War", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E05%20-%20Man%20O%27War.mp4"},
+    {n: "Man from Atlantis S01E06 - Shoot Out At Lands End", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E06%20-%20Shoot%20Out%20At%20Lands%20End.mp4"},
+    {n: "Man from Atlantis S01E07 - Crystal Water Sudden Death", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E07%20-%20Crystal%20Water%20Sudden%20Death.mp4"},
+    {n: "Man from Atlantis S01E08 - The Naked Montague", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E08%20-%20The%20Naked%20Montague.mp4"},
+    {n: "Man from Atlantis S01E09 - CW Hyde", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E09%20-%20CW%20Hyde.mp4"},
+    {n: "Man from Atlantis S01E10 - Scavenger Hunt", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E10%20-%20Scavenger%20Hunt.mp4"},
+    {n: "Man from Atlantis S01E11 - Imp", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E11%20-%20Imp.mp4"},
+    {n: "Man from Atlantis S01E12 - Siren", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E12%20-%20Siren.mp4"},
+    {n: "Man from Atlantis S01E13 - Deadly Carnival", u: "https://archive.org/download/man-from-atlantis-complete-series-1977/Man%20from%20Atlantis%20S01E13%20-%20Deadly%20Carnival.mp4"},
+];
